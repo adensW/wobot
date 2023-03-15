29d149928054234546453681c7aa1168663d0866
wangdingchen <wangdingchen@adens.cn>
2023-03-15 09:37:07 +0800

    feat: 初始化创建项目

---
+++ src/Woo.Wobot/WobotModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace Woo.Wobot;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class WobotModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddSingleton<MainWindow>();
+    }
+}
